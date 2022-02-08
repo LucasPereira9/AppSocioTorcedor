c57581abdf9517ca07089d931845089566afd1c1
Lucas Almeida <lucas.almeida@qesh.ai>
2022-02-08 17:11:33 -0300

    primeiros passos.

---
+++ src/pages/main/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {Container} from './style';
+import {Tela} from './style';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <Container>
+      <Tela>
+        <Text> lallaalallaa</Text>
+      </Tela>
+    </Container>
+  );
+};
+export default App;
---
+++ src/pages/main/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #8b10ae;
+`;
+export const Tela = styled.View`
+  width: 300px;
+  height: 300px;
+  background: #fff;
+  margin: auto;
+`;
+export const Fala = styled.View`
+  width: 70px;
+  height: 60px;
+  color: #8b10ae;
+`;
