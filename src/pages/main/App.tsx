490b3dd5e805da6cc9b7bb3381e7d70ab30d85d5
Lucas Almeida <lucas.almeida@qesh.ai>
2022-02-08 18:07:38 -0300

    ./

--- src/pages/main/App.tsx
+++ src/pages/main/App.tsx
@@ -1,19 +1,10 @@
 /**
  * @format
  */
-
 import React from 'react';
 import {Container} from './style';
-import {Tela} from './style';
-import {Text} from 'react-native';
 
 const App = () => {
-  return (
-    <Container>
-      <Tela>
-        <Text> lallaalallaa</Text>
-      </Tela>
-    </Container>
-  );
+  return <Container />;
 };
 export default App;
