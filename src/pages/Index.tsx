ecbd30c0cb1744a20c7078a6857fac43848c8200
Lucas Almeida <lucas.almeida@qesh.ai>
2022-02-11 18:06:27 -0300

    iniciando trabalhos do footer

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Container} from './style';
 import Header from '../components/Header/Header';
 import {StatusBar} from 'native-base';
+import Footer from '../components/Footer/Footer';
 
 const Index = () => {
   return (
     <Container>
       <StatusBar barStyle="light-content" backgroundColor={'#0b3692'} />
       <Header />
+      <Footer />
     </Container>
   );
 };
