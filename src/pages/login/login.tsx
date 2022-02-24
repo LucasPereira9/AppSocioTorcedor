3e9e07c790a3cd71463cd00989a10601da6ae7b6
Lucas Almeida <lucas.almeida@qesh.ai>
2022-02-24 16:49:09 -0300

    fundo tela de login ok

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import {Container} from './style';
+import {View, Image, StyleSheet} from 'react-native';
 
 const login = () => {
-  return <Container />;
+  return (
+    <Container>
+      <View>
+        <Image
+          style={styles.img}
+          source={require('../../assets/Camp-Nou.jpg')}
+        />
+      </View>
+    </Container>
+  );
 };
 export default login;
+
+const styles = StyleSheet.create({
+  img: {
+    flex: 1,
+    position: 'relative',
+    left: 25,
+  },
+});
